53453e18b721c5708f9854484ef78bb4f5d49946
Rhys Bush <rbush28@eq.edu.au>
2022-09-26 22:33:53 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,26 @@
-minimum_password_length = 10
-is_password_strong = "false"
-user_password = input("Your new password: ")
-while is_password_strong == "false":
-    if len(user_password) >= minimum_password_length:
-        is_password_strong = "true"
-    else:
-        print(f"Password does not meet length requirement (> {minimum_password_length} characters)")
-        user_password = input("Your new password: ")
-print("*" * len(user_password))
+"""Password checking program."""
+
+
+def main():
+    minimum_password_length = 10
+    is_password_strong = "false"
+    user_password = get_password(is_password_strong, minimum_password_length)
+    print_encrypted_password(user_password)
+
+
+def print_encrypted_password(user_password):
+    print("*" * len(user_password))
+
+
+def get_password(is_password_strong, minimum_password_length):
+    user_password = input("Your new password: ")
+    while is_password_strong == "false":
+        if len(user_password) >= minimum_password_length:
+            is_password_strong = "true"
+        else:
+            print(f"Password does not meet length requirement (> {minimum_password_length} characters)")
+            user_password = input("Your new password: ")
+    return user_password
+
+
+main()
