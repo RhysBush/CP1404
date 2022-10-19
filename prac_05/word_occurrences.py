a04ca4d8e73d90eabe4b5834eda1aa886023ae9a
Rhys Bush <rbush28@eq.edu.au>
2022-10-19 22:51:28 +1000

    Add complete prac 5 word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,5 +8,6 @@ for word in words:
 words = list(word_to_occurrence.keys())
 words.sort()
 
+max_length = max(len(word) for word in words)
 for word in words:
-    print(f"{word}: {word_to_occurrence.get(word)}")
+    print(f"{word:{max_length}} : {word_to_occurrence.get(word)}")
