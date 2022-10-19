d1f2a022580d53c1dde22418a2d2b77beffee573
Rhys Bush <rbush28@eq.edu.au>
2022-10-19 22:33:03 +1000

    Complete prac 5 hex colours exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,6 +1,8 @@
-COLOUR_CODES = {"AliceBlue": "#f0f8ff"}
+COLOUR_CODES = {"Brick Red": "#cb4154", "Burnt Orange": "#cc5500", "Lemon Yellow": "#fff44f", "Mint Green": "98ff98",
+                "Alice Blue": "#f0f8ff", "Indigo": "#4b0082", "Violet": "#d02090", "Dutch White": "#efdfbb",
+                "Black Coffee": "#3b2f2f", "Ash Grey": "#b2beb5"}
 
-colour_name = input("Enter a colour name: ")
+colour_name = input("Enter a colour name: ").title()
 while colour_name != "":
     print(f"{colour_name} has a hexadecimal value of {COLOUR_CODES.get(colour_name)}")
-    colour_name = input("Enter a colour name: ")
+    colour_name = input("Enter a colour name: ").title()
