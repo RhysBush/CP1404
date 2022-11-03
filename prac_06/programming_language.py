1f75bcf78354e8ef4c032a6f0c5cd46af416e20b
Rhys Bush <rbush28@eq.edu.au>
2022-11-03 12:51:45 +1000

    Add __init__ and is_dynamic methods to ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,11 @@
+class ProgrammingLanguage:
+
+    def __int__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+    
