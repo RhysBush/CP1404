7a7e470995b947724fc057f2946fd6b680654c7f
Rhys Bush <rbush28@eq.edu.au>
2022-11-10 15:46:03 +1000

    Add completed exercises to prac_07 folder (conflicts between remote + local mains removed prior commits)

---
+++ prac_07/project.py
@@ -0,0 +1,26 @@
+"""
+Estimated time: 20 minutes
+Actual time: 15 minutes
+"""
+
+
+class Project:
+    """Represents a project object."""
+    COMPLETED_THRESHOLD = 100
+
+    def __init__(self, name, start_date, priority=0, cost_estimate=0.0, completion_percentage=0):
+        """Initialize project instance."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return string value for project."""
+        return f"{self.name}, start {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, " \
+               f"estimate ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
+
+    def is_completed(self):
+        """Determine if project is complete"""
+        return self.completion_percentage == self.COMPLETED_THRESHOLD
