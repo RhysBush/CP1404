92ec1006428f7e5409050830355638c252980013
Rhys Bush <rbush28@eq.edu.au>
2022-11-16 23:47:09 +1000

    Add completed miles to kilometres converter exercises

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,19 +1,41 @@
 """
 Rhys Bush - Miles to Kilometres Converter
 Expected time: 20 minutes.
-Actual time: minutes.
+Actual time: 40 minutes.
 """
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.app import StringProperty
+
+MILES_TO_KM_FACTOR = 1.609
 
 
 class MilesToKilometresConverter(App):
+    output_km = StringProperty()
 
     def build(self):
         self.title = 'Convert Miles to Kilometres'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_calculation(self, text):
+        miles = self.convert_to_number(text)
+        self.update_result(miles)
+
+    def handle_increment(self, text, change):
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+
+    def update_result(self, miles):
+        self.output_km = str(miles * MILES_TO_KM_FACTOR)
+
+    @staticmethod  # Taken from solution - removes static error
+    def convert_to_number(text):
+        try:
+            return float(text)
+        except ValueError:
+            return 1.0
 
 
 MilesToKilometresConverter().run()
