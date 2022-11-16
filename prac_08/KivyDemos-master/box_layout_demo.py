e9232000bd37a5026b02b0e7e3a13c48bc823a4e
Rhys Bush <rbush28@eq.edu.au>
2022-11-16 20:49:58 +1000

    Add completed box_layout exercises to prac_08 directory

--- prac_08/KivyDemos-master/box_layout_demo.py
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -10,6 +10,11 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         print('greet')
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def clear_fields(self):
+        self.root.ids.output_label.text = ''
+        self.root.ids.input_name.text = ''
 
 
 BoxLayoutDemo().run()
